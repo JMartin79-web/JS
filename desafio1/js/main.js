2ee97e510819cda429f5a9ce6486f7170b4b2940
martin <jmartinaguirre79@gmail.com>
2022-07-17 20:36:40 -0300

    primer commit

---
+++ desafio1/js/main.js
@@ -0,0 +1,8 @@
+//CREAR ALGORITMO UTILIZANDO UN CICLO
+//Pedir un número mediante prompt y sumarle otro número en cada repetición, realizando una salida por cada resultado.
+
+let num = (parseInt(prompt("ingresá un número")))
+
+for(let num; num < 11; num ++){
+    alert(num)
+}
